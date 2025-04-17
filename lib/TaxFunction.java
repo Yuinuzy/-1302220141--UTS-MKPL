dada23f4ff13b39901284b0aa5e52b6152b8b27c
Yuinuzy <tsaqifzayyan123@gmail.com>
2025-04-17 21:58:08 +0700

    menghapus comment pada TaxFunction, dan mungkin ada yang belum kepush dari commit sebelumnya

--- lib/TaxFunction.java
+++ lib/TaxFunction.java
@@ -1,20 +1,6 @@
 package uts_makepal.lib;
 
 public class TaxFunction {
-
-	
-	/**
-	 * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun.
-	 * 
-	 * Pajak dihitung sebagai 5% dari penghasilan bersih tahunan (gaji dan pemasukan bulanan lainnya dikalikan jumlah bulan bekerja dikurangi pemotongan) dikurangi penghasilan tidak kena pajak.
-	 * 
-	 * Jika pegawai belum menikah dan belum punya anak maka penghasilan tidak kena pajaknya adalah Rp 54.000.000.
-	 * Jika pegawai sudah menikah maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000.
-	 * Jika pegawai sudah memiliki anak maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000 per anak sampai anak ketiga.
-	 * 
-	 */
-	
-	
     private static final int PEMASUKAN_BEBAS_PAJAK = 54_000_000;
     private static final int BONUS_BEBAS_PAJAK_PASANGAN = 4_500_000;
     private static final int BONUS_BEBAS_PAJAK_PER_ANAK = 1_500_000;
