6dde52808f4c07c93b4fb07b6c65c3ce50f9c7e4
Yuinuzy <tsaqifzayyan123@gmail.com>
2025-04-17 21:22:56 +0700

    mengubah statement if menjadi switch case statement

--- lib/Gaji.java
+++ lib/Gaji.java
@@ -20,22 +20,23 @@ public class Gaji {
     }
     
     public void setMonthlySalary(int grade) {	
-		if (grade == 1) {
-			monthlySalary = 3000000;
+                switch(grade)   {
+                    case 1:
+                        monthlySalary = 3000000;
 			if (isForeigner) {
 				monthlySalary = (int) (3000000 * 1.5);
 			}
-		}else if (grade == 2) {
-			monthlySalary = 5000000;
+                    case 2:
+                        monthlySalary = 5000000;
 			if (isForeigner) {
 				monthlySalary = (int) (3000000 * 1.5);
 			}
-		}else if (grade == 3) {
-			monthlySalary = 7000000;
+                    case 3:
+                        monthlySalary = 7000000;
 			if (isForeigner) {
 				monthlySalary = (int) (3000000 * 1.5);
 			}
-		}
+                }
 	}
 	
 	public void setAnnualDeductible(int deductible) {	
